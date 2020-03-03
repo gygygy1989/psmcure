851b38a65930f9dbccf8150e1f793942deb03914
ENNAN GU <ennangu@ENNANs-MacBook-Pro.local>
2020-03-03 16:58:18 -0500

    Update on 03/03/2020.

---
+++ R/breastla.R
@@ -0,0 +1,16 @@
+#' Breast Cancer Dataset for Louisiana
+#'
+#' Pseudo data from SEER 2000-2012 Louisiana female breast cancer,
+#' for regional stage, with 6200 observations, mean age of 65.7.
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage data(breastla)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+"breastla"
